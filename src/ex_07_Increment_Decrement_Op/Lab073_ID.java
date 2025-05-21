b039dd832534239b59ea0dd88a0402a53ecbdd22
divisusagar <divisusagar@gmail.com>
2025-05-21 16:58:57 +0530

    Commit-1 on 21 May

---
+++ src/ex_07_Increment_Decrement_Op/Lab073_ID.java
@@ -0,0 +1,11 @@
+package ex_07_Increment_Decrement_Op;
+
+public class Lab073_ID {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int result = a++;
+        System.out.println(a);      // 11
+        System.out.println(result); // 10
+    }
+}
